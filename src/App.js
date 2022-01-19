5a836845e18e95f85f08a74de43e6f58d5239de8
zhucheng <zhucheng@agree.com.cn>
2022-01-20 00:36:06 +0800

    wip: 在 app 中添加组件

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const el = <h1>Hello react!!!</h1>;
+
+// function timeCount() {
+//   const el = <h3>现在时间: {new Date().toLocaleTimeString()}</h3>;
+
+//   ReactDOM.render(el, document.getElementById('root'));
+// }
+
+// setInterval(() => {
+//   timeCount();
+// }, 1000);
+
+// ========================================
+
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+function Page() {
+  return (
+    <div>
+      <Welcome name='Juche' />
+      <Welcome name='Juching' />
+    </div>
+  );
+}
+
+// function Clock(props) {
+//   return (
+//     <div className='Clock'>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div className='Clock'>
+        <h1>Hello, world!</h1>
+        <h2>It's {this.props.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Clock date={new Date()} />
+      <Page />
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          7k Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- src/index.js
+++ src/index.js
@@ -4,63 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// ========================================
-
-// const el = <h1>Hello react!!!</h1>;
-
-// function timeCount() {
-//   const el = <h3>现在时间: {new Date().toLocaleTimeString()}</h3>;
-
-//   ReactDOM.render(el, document.getElementById('root'));
-// }
-
-// setInterval(() => {
-//   timeCount();
-// }, 1000);
-
-// ========================================
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
-
-function Page() {
-  return (
-    <div>
-      <Welcome name='Juche' />
-      <Welcome name='Juching' />
-    </div>
-  );
-}
-
-const el = <Page />;
-
-ReactDOM.render(el, document.getElementById('root'));
-
-// ========================================
-
-// ========================================
-
-// ========================================
-
-// ========================================
-
-// ========================================
-
-// ========================================
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
